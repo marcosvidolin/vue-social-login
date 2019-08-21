30bdb15578f0fe30e3fedee59cfcb988af19a53c
marcosvidolin <marcosvidolin@gmail.com>
2019-08-21 01:04:05 -0300

    Bulma setup

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,20 @@
 <template>
   <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <hello></hello>
-    </main>
+    <navbar></navbar>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Navbar from './components/Navbar'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Navbar
   }
 }
 </script>
 
 <style>
-body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
+@import "~bulma/css/bulma.css";
 </style>
