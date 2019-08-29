bb7acbc22a8596a477fe6e7318c8e77c1d96ed53
marcosvidolin <marcosvidolin@gmail.com>
2019-08-29 00:43:38 -0300

    Navbar burguer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -33,7 +33,24 @@
 
 <script>
 export default {
-  name: 'navbar'
+  name: 'navbar',
+  mounted: function () {
+    document.addEventListener('DOMContentLoaded', () => {
+      const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+      if ($navbarBurgers.length > 0) {
+        $navbarBurgers.forEach(el => {
+          el.addEventListener('click', () => {
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target
+            const $target = document.getElementById(target)
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle('is-active')
+            $target.classList.toggle('is-active')
+          })
+        })
+      }
+    })
+  }
 }
 </script>
 
