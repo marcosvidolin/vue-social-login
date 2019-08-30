806412e4595acef6eb365cb454d34a9a913f1a26
Marcos Vidolin <marcosvidolin@gmail.com>
2019-08-30 19:19:19 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,31 @@
 <template>
   <div id="app">
-    <navbar></navbar>
-
-    <div class="section has-background-light">
-      <div class="columns">
-        
-        <aside class="column is-3 is-hidden-mobile">
-          <sidebar></sidebar>
-        </aside>
-
-        <main class="column">
-
-          <main-content></main-content>
-        </main>
-
-      </div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
-
-    <!--div class="columns">
-      <div class="column is-2">
-        <meno></meno>
-      </div>
-      <div class="column is-10">
-        <div class="container column">
-          <section>
-            <perfil-loja-card></perfil-loja-card>
-          </section>
-        </div>
-      </div>
-    </div-->
-
-
-    <rodape></rodape>
-
+    <router-view/>
   </div>
 </template>
 
-
-<script>
-import Navbar from './components/Navbar'
-import Login from './components/Login'
-import PerfilLojaCard from './components/PerfilLojaCard'
-import Sidebar from './components/Sidebar'
-import MainContent from './components/MainContent'
-import Rodape from './components/Rodape'
-
-export default {
-  name: 'app',
-  components: {
-    Navbar,
-    Login,
-    PerfilLojaCard,
-    Sidebar,
-    MainContent,
-    Rodape
-  }
-}
-</script>
-
 <style>
-@import "~bulma/css/bulma.css";
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
