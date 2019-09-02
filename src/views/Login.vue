8de853eba03f45de3c60dc4975744028ba0e52e1
Marcos Vidolin <marcosvidolin@gmail.com>
2019-09-02 11:28:39 -0300

    Basic login page

---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="hero has-background-white is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <div class="notification has-background-white">
+          <h1 class="title">Login</h1>
+
+          <h4 class="subtitle">
+            A basic social login PWA project using
+            <strong>Vue</strong>,
+            <strong>Firebase</strong> and
+            <strong>Bulma</strong>.
+          </h4>
+
+          <div class="notification has-background-white">
+            <a class="button is-outlined is-danger is-rounded is-medium is-fullwidth">
+              <span class="icon">
+                <i class="fab fa-instagram"></i>
+              </span>
+              <span>Sign in with Instagram</span>
+            </a>
+            <br />
+            <a class="button is-google is-outlined is-info is-rounded is-medium is-fullwidth">
+              <span class="icon">
+                <i class="fab fa-google"></i>
+              </span>
+              <span>Sign in with Google</span>
+            </a>
+          </div>
+          <!-- /Notification -->
+        </div>
+        <!-- /Notification -->
+      </div>
+      <!-- /Container -->
+    </div>
+    <!-- /Hero Body -->
+  </section>
+</template>
