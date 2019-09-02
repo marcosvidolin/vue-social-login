11bacc936dcc3e81e8c0da39b44d6e711dac9e63
Marcos Vidolin <marcosvidolin@gmail.com>
2019-09-02 10:31:17 -0300

    Add Bulma

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,16 @@
     <div id="nav">
       <p>Have a cup of coffee: <i class="fas fa-coffee"></i> </p>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/login">Login</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+@import "~bulma/css/bulma.css";
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
