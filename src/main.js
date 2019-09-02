293aa3e4d08a3f4fdb0a9a562eec6ee1a5929022
Marcos Vidolin <marcosvidolin@gmail.com>
2019-09-02 02:14:54 -0300

    Initialize Firebase

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SEND_ID"
+});
+
 new Vue({
   router,
   render: h => h(App)
