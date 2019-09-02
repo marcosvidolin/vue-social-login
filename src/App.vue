6599b0d251fed073d8526a95da8a163f3052cf04
Marcos Vidolin <marcosvidolin@gmail.com>
2019-09-02 11:55:11 -0300

    Remove unused code

--- src/App.vue
+++ src/App.vue
@@ -6,36 +6,11 @@
             integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" 
             crossorigin="anonymous">
 
-    <div id="nav">
-      <p>Have a cup of coffee: <i class="fas fa-coffee"></i> </p>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/login">Login</router-link>
-    </div>
     <router-view/>
+
   </div>
 </template>
 
 <style>
 @import "~bulma/css/bulma.css";
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
