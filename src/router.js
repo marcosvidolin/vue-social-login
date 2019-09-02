dfc567bd2b48de577d95ec0c16083e0208fc249a
Marcos Vidolin <marcosvidolin@gmail.com>
2019-09-02 11:54:43 -0300

    Access the app only with authentication

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 
@@ -10,22 +11,25 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '*',
+      redirect: '/login'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/',
+      redirect: '/login'
     },
     {
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
