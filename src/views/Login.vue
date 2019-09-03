eaad7019bee9872fd375c2bdb9c6c83e1bdf4ac3
Marcos Vidolin <marcosvidolin@gmail.com>
2019-09-02 23:04:22 -0300

    Login adjusts

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,33 +2,32 @@
   <section class="hero has-background-white is-fullheight">
     <div class="hero-body">
       <div class="container">
-        <div class="notification has-background-white">
+        <div class="notification has-background-white has-text-centered">
           <h1 class="title">Login</h1>
+          <br>
 
-          <h4 class="subtitle">
+          <p>
             A basic social login PWA project using
             <strong>Vue</strong>,
             <strong>Firebase</strong> and
             <strong>Bulma</strong>.
-          </h4>
+          </p>
+          <br>
 
-          <div class="notification has-background-white">
-            <a class="button is-outlined is-danger is-rounded is-medium is-fullwidth">
-              <span class="icon">
-                <i class="fab fa-instagram"></i>
-              </span>
-              <span>Sign in with Instagram</span>
-            </a>
-            <br />
-            <a class="button is-google is-outlined is-info is-rounded is-medium is-fullwidth"
-                @click="googleLogin">
-              <span class="icon">
-                <i class="fab fa-google"></i>
-              </span>
-              <span>Sign in with Google</span>
-            </a>
-          </div>
-          <!-- /Notification -->
+          <a class="button is-outlined is-danger is-rounded is-medium is-fullwidth">
+            <span class="icon">
+              <i class="fab fa-instagram"></i>
+            </span>
+            <span>Sign in with Instagram</span>
+          </a>
+          <br />
+          <a class="button is-google is-outlined is-info is-rounded is-medium is-fullwidth"
+            @click="loginWithGoogle">
+            <span class="icon">
+              <i class="fab fa-google"></i>
+            </span>
+            <span>Sign in with Google</span>
+          </a>
         </div>
         <!-- /Notification -->
       </div>
@@ -39,20 +38,24 @@
 </template>
 
 <script>
-import firebase from 'firebase';
+import firebase from "firebase";
 
 export default {
-    name: 'login',
-    methods: {
-        googleLogin() {
-            const provider = new firebase.auth.GoogleAuthProvider();
-            firebase.auth().signInWithPopup(provider).then(() => {
-                this.$router.replace('home');
-            }).catch((err) => {
-                // TODO:
-                alert(err.message);
-            });
-        }
+  name: "login",
+  methods: {
+    loginWithGoogle() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(() => {
+          this.$router.replace("home");
+        })
+        .catch(err => {
+          // TODO:
+          alert(err.message);
+        });
     }
-}
+  }
+};
 </script>
