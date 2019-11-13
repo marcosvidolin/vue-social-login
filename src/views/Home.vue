8caa1f45293ed28713b4e6a5fc482518baaebdbb
Regivaldo Silva <regivaldo@ciandt.com>
2019-11-13 09:24:43 -0300

    Created a card with photo, name and email of user

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="home has-background-white-ter">
     <my-navbar></my-navbar>
+
     <div class="section">
-      <figure class="image is-128x128">
-        <img v-bind:src="user.photoURL" />
-      </figure>
-      <div class="container">{{ user.displayName }}</div>
+      <div class="columns is-centered">
+        <div class="column is-3">
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">Authenticated User</p>
+            </header>
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <figure class="image is-48x48">
+                    <img v-bind:src="user.photoURL" v-bind:alt="user.displayName"/>
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <p class="title is-4">{{ user.displayName }}</p>
+                  <p class="subtitle is-6">{{ user.email }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>      
     </div>
   </div>
 </template>
